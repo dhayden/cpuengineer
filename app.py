df2d4d00fb381476097d86b992fa57a2e19fb066
dhayden <cdhayden25@gmail.com>
2024-04-23 10:32:27 -0500

    added pages

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'cpuengineer'
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
